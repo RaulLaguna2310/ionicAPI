10674ce6c008b24ced1b143060363ef2c019a1aa
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-29 11:31:56 -0300

    ionicAPI

---
+++ src/app/filme-detalhe/filme-detalhe.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-filme-detalhe',
+  templateUrl: './filme-detalhe.page.html',
+  styleUrls: ['./filme-detalhe.page.scss'],
+})
+export class FilmeDetalhePage implements OnInit {
+  film: any;
+
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.http.get(`https://swapi.dev/api/film/${id}`).subscribe(res => {
+      this.film = res;
+    });
+  }
+
+}
